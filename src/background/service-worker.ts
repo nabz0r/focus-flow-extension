8770cacb684dcbc8a4e106c2a1284abf9727d165
nabilux <nabz0r@gmail.com>
2025-01-07 11:08:33 +0100

    feat: integrate timer core with service worker

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -1,47 +1,22 @@
+import { TimerCore } from './timer-core';
 import { SiteBlocker } from './site-blocker';
 
-class FocusFlowWorker {
-  private siteBlocker: SiteBlocker;
+class FocusFlowService {
+  private timer: TimerCore;
+  private blocker: SiteBlocker;
 
   constructor() {
-    this.siteBlocker = new SiteBlocker();
-    this.initListeners();
+    this.timer = new TimerCore();
+    this.blocker = new SiteBlocker();
+    this.loadInitialState();
   }
 
-  private initListeners() {
-    chrome.runtime.onInstalled.addListener(this.handleInstall);
-    chrome.alarms.onAlarm.addListener(this.handleAlarm);
-  }
-
-  private handleInstall = (details: chrome.runtime.InstalledDetails) => {
-    if (details.reason === 'install') {
-      this.initStorage();
+  private async loadInitialState() {
+    const data = await chrome.storage.local.get(['config']);
+    if (data.config) {
+      // Initialize avec config sauvegardée
     }
-  }
-
-  private handleAlarm = (alarm: chrome.alarms.Alarm) => {
-    if (alarm.name === 'focusSession') {
-      this.notifySessionEnd();
-    }
-  }
-
-  private async initStorage() {
-    const defaultSettings = {
-      timer: 25,
-      break: 5,
-      blockedSites: []
-    };
-    await chrome.storage.local.set({ settings: defaultSettings });
-  }
-
-  private async notifySessionEnd() {
-    await chrome.notifications.create('sessionEnd', {
-      type: 'basic',
-      iconUrl: 'assets/icon128.png',
-      title: 'Focus Session Complete!',
-      message: 'Time for a break'
-    });
   }
 }
 
-new FocusFlowWorker();
+new FocusFlowService();
